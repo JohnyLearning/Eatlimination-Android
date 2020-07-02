8afb0ef2d80bdb2a00f8b2c84b97fde8ef73ab71
Ivan Hadzhi <ivan.hadzhiiliev@scotiabank.com>
2020-07-02 16:29:59 -0400

    update colors + databinding fixed on home fragment

---
+++ app/src/main/java/com/ihadzhi/eatlimination/HomeFragment.java
@@ -0,0 +1,51 @@
+package com.ihadzhi.eatlimination;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.ihadzhi.eatlimination.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding dataBinding;
+
+    public static HomeFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        HomeFragment fragment = new HomeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        setHasOptionsMenu(true);
+        return dataBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        dataBinding.addFoodAction.setOnClickListener(button -> {
+            Toast.makeText(getActivity(), "CLicked", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
--- app/src/main/java/com/ihadzhi/eatlimination/StartActivity.java
+++ app/src/main/java/com/ihadzhi/eatlimination/StartActivity.java
@@ -1,6 +1,9 @@
 package com.ihadzhi.eatlimination;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -9,6 +12,16 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
+        setupHome();
+    }
+
+    private void setupHome() {
+        Fragment homeFragment = HomeFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.content_frame, homeFragment, homeFragment.getClass().getCanonicalName())
+                .addToBackStack(homeFragment.getClass().getCanonicalName())
+                .commit();
     }
 }
