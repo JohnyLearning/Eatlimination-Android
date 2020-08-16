1df3268643afabd2bfbf61cee9fb71df48599d67
Ivan Hadzhi <ivan.hadzhiiliev@scotiabank.com>
2020-08-16 16:38:24 -0400

    symptoms ui adjustments

--- app/src/main/java/com/ihadzhi/eatlimination/ui/SymptomRecordFragment.java
+++ app/src/main/java/com/ihadzhi/eatlimination/ui/SymptomRecordFragment.java
@@ -1,11 +1,13 @@
 package com.ihadzhi.eatlimination.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
@@ -16,6 +18,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ihadzhi.eatlimination.R;
+import com.ihadzhi.eatlimination.data.SymptomRecord;
 import com.ihadzhi.eatlimination.databinding.SymptomRecordFragmentBinding;
 import com.ihadzhi.eatlimination.databinding.SymptomsFragmentBinding;
 import com.ihadzhi.eatlimination.viewmodel.SymptomRecordViewModel;
@@ -51,6 +54,35 @@ public class SymptomRecordFragment extends BaseFragment {
         setTitle(getString(R.string.new_recording_title, "High blood pressure"));
         dataBinding.symptomImage.setImageDrawable(getContext().getDrawable(R.drawable.hbp));
         dataBinding.symptomDescription.setText("Quick zephyrs blow, vexing daft Jim. Sphinx of black quartz, judge my vow. Two driven jocks help fax my big quiz. Five quacking zephyrs jolt my wax bed. The five boxing wizards jump quickly.");
+        dataBinding.greenStatus.setOnClickListener(v -> changesSymptomStatus(SymptomRecord.SymptomCategory.green));
+        dataBinding.yellowStatus.setOnClickListener(v -> changesSymptomStatus(SymptomRecord.SymptomCategory.yellow));
+        dataBinding.redStatus.setOnClickListener(v -> changesSymptomStatus(SymptomRecord.SymptomCategory.red));
+    }
+
+    private void changesSymptomStatus(SymptomRecord.SymptomCategory category) {
+        switch (category) {
+            case green:
+                dataBinding.greenStatus.setBackground(getDrawable(R.drawable.symptom_status_green_background));
+                dataBinding.redStatus.setBackground(getDrawable(R.drawable.red_status_inactive));
+                dataBinding.yellowStatus.setBackground(getDrawable(R.drawable.yellow_status_inactive));
+                break;
+            case yellow:
+                dataBinding.greenStatus.setBackground(getDrawable(R.drawable.green_status_inactive));
+                dataBinding.redStatus.setBackground(getDrawable(R.drawable.red_status_inactive));
+                dataBinding.yellowStatus.setBackground(getDrawable(R.drawable.symptom_status_yellow_background));
+                break;
+            case red:
+                dataBinding.greenStatus.setBackground(getDrawable(R.drawable.green_status_inactive));
+                dataBinding.redStatus.setBackground(getDrawable(R.drawable.symptom_status_red_background));
+                dataBinding.yellowStatus.setBackground(getDrawable(R.drawable.yellow_status_inactive));
+                break;
+            default:
+                // no-op
+        }
+    }
+
+    private Drawable getDrawable(@DrawableRes int drawable) {
+        return getContext().getDrawable(drawable);
     }
 
     @Override
@@ -61,5 +93,7 @@ public class SymptomRecordFragment extends BaseFragment {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 
 }
